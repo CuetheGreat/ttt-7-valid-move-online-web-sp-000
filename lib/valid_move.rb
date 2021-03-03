ae88f2644ebf9eda74ce79929c806c10995dd585
Clayon Stewart <c.stewart89@gmail.com>
2021-03-03 02:09:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,12 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if !position_taken?(board,index) || index < 0 || index > 8
+  if(index < 0 || index > 8)
+    return nil
+  if !position_taken?(board,index)
     return true
-  else return false
+  else
+    return false
   end
 end
     
